48d4b71db1fc4ec1808f1eeec96754e0bf781dc6
SalmonTheWise <raisa.chetverikova@gmail.com>
2015-11-07 21:30:20 +0300

    done

--- discr_math/Knuth–Morris–Pratt.py
+++ discr_math/Knuth–Morris–Pratt.py
@@ -1,9 +1,9 @@
 def kmp(a, b):
     index = -1
-    for i in xrange(len(a) - len(b)+1):
+    for i in range(len(a) - len(b)+1):
         success = True
-        for j in xrange(len(b)):
-            if b[j] <> a[i+j]:
+        for j in range(len(b)):
+            if b[j] != a[i+j]:
                 success = False
                 break
         if success:
