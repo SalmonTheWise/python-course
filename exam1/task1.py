3394b9dc4e4baf7f0c1dd3235c873a5a5b0c8aa0
SalmonTheWise <raisa.chetverikova@gmail.com>
2015-11-07 17:48:09 +0300

    task1

--- exam1/task1.py
+++ exam1/task1.py
@@ -1,1 +1,11 @@
-__author__ = 'jack-a-lynn'
+ls = ['yo']
+inp = open('yazkora.txt', 'r')
+out = open('answer.txt', 'w')
+for line in inp:
+    for sentence in line.split('.'):
+        for word in sentence.split(' '):
+            if word[(len(word) - 1):len(word)] in ls:
+                    out.write(word + ' '
+                              )
+inp.close()
+out.close()
