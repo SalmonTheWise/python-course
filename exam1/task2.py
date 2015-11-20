94b17befc46fc608c4a339a130e555f63503be95
SalmonTheWise <raisa.chetverikova@gmail.com>
2015-11-20 21:33:10 +0300

    task2 correction

--- exam1/task2.py
+++ exam1/task2.py
@@ -1,5 +1,17 @@
-inp = open('dict.txt', 'r')
-noun = set()
-adjective = set()
-verb = set()
-#don't know what else to do
+inp = open("dict.txt", "r")
+noun = 0
+adj = 0
+verb = 0
+for line in inp:
+    for word in line.split('\n'):
+        if word[(len(word) - 2):len(word)] == "ka":
+            noun += 1
+        elif word[(len(word) - 2):len(word)] == "yo":
+            adj += 1
+        else:
+            verb += 1
+inp.close()
+cool_comb = (1 ** adj + 2 ** adj + 3 ** adj + 4 ** adj + 5 ** adj + 6 ** adj + 7 ** adj)*noun*verb
+print(cool_comb)
+
+
--- homeworks/classwork/regular.py
+++ homeworks/classwork/regular.py
@@ -1,3 +1,5 @@
 __author__ = 'jack-a-lynn'
 
 import re
+lst = re.findall("\w*yo", "hoyo stameska kopyo yo")
+print(lst)
