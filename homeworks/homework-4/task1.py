b3f23cd9dcc515d57213962ec47efe0cba750b25
SalmonTheWise <raisa.chetverikova@gmail.com>
2015-11-22 19:37:53 +0300

    task1 corrected again

--- homeworks/homework-4/task1.py
+++ homeworks/homework-4/task1.py
@@ -1,14 +1,14 @@
 ls = ['yo']
-with open('yazkora.txt', 'r') as inp:
-    with open('answer.txt', 'w') as out:
-        text = inp.read().replace("\n", " ")
-        sentences = text.split(".")
-        for sentence in sentences:
-            adj = str()
-            words = sentence.split(' ')
-            for word in words:
-                if word[(len(word) - 2):len(word)] in ls:
-                    adj += word + " "
-            out.write(adj + "\n")
+inp = open('yazkora.txt', 'r')
+out = open('answer.txt', 'w')
+text = inp.read().replace("\n", " ")
+sentences = text.split(".")
+for sentence in sentences:
+    adj = str()
+    words = sentence.split(' ')
+    for word in words:
+        if word[(len(word) - 2):len(word)] in ls:
+            adj += word + " "
+    out.write(adj + "\n")
 inp.close()
 out.close()
