4e7b70129a6e4d499e865dd8a589c4be90d0e3df
SalmonTheWise <raisa.chetverikova@gmail.com>
2015-12-05 13:30:53 +0300

    task3 add

--- exam2/task3.py
+++ exam2/task3.py
@@ -2,11 +2,10 @@ __author__ = 'jack-a-lynn'
 import requests
 from lxml import etree
 
-url = "https://twitter.com/googlefacts"
-data = requests.get(url).text
+url = requests.get('https://twitter.com/googlefacts').text
 
 parser = etree.HTMLParser()
 tree = etree.fromstring(data, parser)
 
-for element in tree.iter("tweet"):
-    print(element.attrib["TweetTextSize TweetTextSize--16px js-tweet-text tweet-text"])
+for element in tree.iter("p"):
+    if "class" in tweet.attrib:
