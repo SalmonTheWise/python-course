d9db982b14eafdd909380af2866d87a32cd76500
SalmonTheWise <raisa.chetverikova@gmail.com>
2016-02-26 21:26:33 +0300

    task0

---
+++ homework-spring/hw1/task0.py
@@ -0,0 +1,10 @@
+__author__ = 'jack-a-lynn'
+
+def sum(a, b):
+    if type(a) == int and type(b) == int:
+        if a >= 0 and b >= 0:
+            return int(a) + int(b)
+        else:
+            raise ValueError
+    else:
+        raise TypeError
